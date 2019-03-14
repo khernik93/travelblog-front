eaf01771ab68a4eb1c669a565d41b32131cb1987
Karol Hernik <khernik93@gmail.com>
2019-03-14 11:56:02 +0100

    Add swiper feature back again

--- src/modules/header/components/swiper/swiper.component.ts
+++ src/modules/header/components/swiper/swiper.component.ts
@@ -1,17 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
+import { SwiperDirective } from 'ngx-swiper-wrapper';
 import { PostContentDTO } from '../../../../shared/clients/api/api.model';
+import { SwiperService } from '../../containers/swiper/swiper.service';
 
 @Component({
   selector: 'swiper-component',
   styleUrls: ['./swiper.component.scss'],
   templateUrl: './swiper.component.html'
 })
-export class SwiperComponent {
+export class SwiperComponent implements OnChanges {
 
   @Input() photosForSelectedTab: string[];
   @Input() post$: Observable<PostContentDTO>;
+  @ViewChild(SwiperDirective) directiveRef: SwiperDirective;
+  config = this.swiperService.CONFIGURATION;
 
-  constructor() { }
+  constructor(
+    private swiperService: SwiperService
+  ) { }
+
+  ngOnChanges() {	
+    this.reinitializeSwiper();	
+  }
+
+  /**	
+   * Initializes swiper for the first time if not yet done, or updates photos	
+   * on state change:	
+   * 	
+   * call update() function on the Swiper object	
+   */	
+  private reinitializeSwiper(): void {	
+    this.directiveRef.update();
+  }
 
 }
---
+++ src/modules/header/containers/swiper/swiper.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';	
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';	
+
+@Injectable()	
+export class SwiperService {	
+
+   /**	
+   * Configuration object for initializing swiper element	
+   */	
+  public CONFIGURATION: SwiperConfigInterface = {	
+    slidesPerView: 1,	
+    spaceBetween: 0,	
+    loop: true,	
+    speed: 300,	
+    preloadImages: true,	
+    pagination: {	
+      el: `.swiper-pagination`	
+    },	
+    navigation: {	
+      nextEl: `.swiper-button-next`,	
+      prevEl: `.swiper-button-prev`	
+    }	
+  };	
+
+ }
